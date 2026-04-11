516423a7b55613c776cb656726312539a0de4799
li389954418 <50436736+li389954418@users.noreply.github.com>
2026-04-11 21:05:02 +0800

    修改vity报错
    修改vity报错

--- app/src/main/java/com/example/nettool/MainActivity.kt
+++ app/src/main/java/com/example/nettool/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
