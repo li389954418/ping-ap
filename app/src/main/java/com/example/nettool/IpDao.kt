b14ad0d88cae960a1d3704dbad2ce534e32a06ae
li389954418 <50436736+li389954418@users.noreply.github.com>
2026-04-12 00:28:40 +0800

    更新 IpDao.kt

--- app/src/main/java/com/example/nettool/IpDao.kt
+++ app/src/main/java/com/example/nettool/IpDao.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -13,6 +14,9 @@ interface IpDao {
 
     @Insert
     suspend fun insert(entry: IpEntry)
+
+    @Update
+    suspend fun update(entry: IpEntry)
 
     @Delete
     suspend fun delete(entry: IpEntry)
