71bc161d106cd169f401be805ce89fdc44b66a14
li389954418 <50436736+li389954418@users.noreply.github.com>
2026-04-11 23:51:20 +0800

    Update AppDatabase.kt

--- app/src/main/java/com/example/nettool/AppDatabase.kt
+++ app/src/main/java/com/example/nettool/AppDatabase.kt
@@ -4,8 +4,10 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 
-@Database(entities = [IpEntry::class], version = 1)
+@Database(entities = [IpEntry::class], version = 2)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun ipDao(): IpDao
 
@@ -13,13 +15,20 @@ abstract class AppDatabase : RoomDatabase() {
         @Volatile
         private var INSTANCE: AppDatabase? = null
 
+        private val MIGRATION_1_2 = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("ALTER TABLE ip_table ADD COLUMN extraRemarks TEXT NOT NULL DEFAULT '{}'")
+            }
+        }
+
         fun getInstance(context: Context): AppDatabase {
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     AppDatabase::class.java,
                     "ip_database"
-                ).build()
+                ).addMigrations(MIGRATION_1_2)
+                 .build()
                 INSTANCE = instance
                 instance
             }
