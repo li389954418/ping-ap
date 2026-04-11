8255c222b6ea30f5da0b9da9d3b472d8ae3d564a
li389954418 <50436736+li389954418@users.noreply.github.com>
2026-04-11 23:51:03 +0800

    Update IpEntry.kt

--- app/src/main/java/com/example/nettool/IpEntry.kt
+++ app/src/main/java/com/example/nettool/IpEntry.kt
@@ -7,6 +7,7 @@ import androidx.room.PrimaryKey
 data class IpEntry(
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
-    val name: String,
-    val address: String
+    val name: String,                // 主备注名称
+    val address: String,             // IP或域名
+    val extraRemarks: String = "{}"  // 额外备注字段，JSON格式存储键值对
 )
